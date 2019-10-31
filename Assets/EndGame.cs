1496076bd03c4b52242c1eb332623ab8fc5f4272
Jonathan Allen <joal7299@colorado.edu>
2019-10-30 19:38:01 -0600

    Game over scene

---
+++ Assets/EndGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+
+    public int Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        //all projectile colliding game objects should be tagged "Enemy" or whatever in inspector but that tag must be reflected in the below if conditional
+        if (col.gameObject.tag == "Enemy")
+        {
+            Health--;
+            Debug.Log("hit exit");
+
+            if(Health <= 0)
+            {
+                Debug.Log("quit");
+                SceneManager.LoadScene("GameOver");
+            }
+        }
+
+    }
+}
