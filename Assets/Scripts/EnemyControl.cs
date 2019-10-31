c14bb13457895afa00529b0d64517e48540eee60
Liam Slyne <liam.slyne@colorado.edu>
2019-10-30 19:39:24 -0600

    combo1

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -14,11 +14,24 @@ public class EnemyControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        int randomIndex = Random.Range(1, 4);
+
         ExitPosition = GameObject.FindGameObjectWithTag("Exit").transform;
 
         StartPosition = GameObject.FindGameObjectWithTag("Entrance").transform;
-
-        transform.position = new Vector3(11.0f, 1.0f, 1.0f);
+        if(randomIndex == 1)
+        {
+            transform.position = new Vector3(-190.0f, 0.2f, -1.0f);
+        }
+        else if(randomIndex == 2)
+        {
+            transform.position = new Vector3(-190.0f, 0.2f, 1.0f);
+        }
+        else if(randomIndex == 3)
+        {
+            transform.position = new Vector3(-190.0f, 0.2f, 3.0f);
+        }
     }
 
     // Update is called once per frame
