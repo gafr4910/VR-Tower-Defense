e699b8508d5ac73aa3993461aeff0818d0b98519
Jonathan Allen <joal7299@colorado.edu>
2019-12-12 17:37:45 -0700

    different zombie/weapons tests

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -10,12 +10,17 @@ public class EnemyControl : MonoBehaviour
     public Transform ExitPosition;
     public Transform StartPosition;
 
+    public GameObject player;
+
     private Transform EnemyPos;
     // Start is called before the first frame update
     void Start()
     {
 
         int randomIndex = Random.Range(1, 4);
+
+        
+        
 
         ExitPosition = GameObject.FindGameObjectWithTag("Exit").transform;
 
@@ -37,7 +42,11 @@ public class EnemyControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        player = GameObject.Find("OVRPlayerController");
+        CameraMovement pCam = player.GetComponent<CameraMovement>();
+
+        pCam.damaged = true;
+
         if (health > 0)
         {
             transform.position = Vector3.MoveTowards(transform.position, ExitPosition.localPosition, speed);
@@ -46,5 +55,16 @@ public class EnemyControl : MonoBehaviour
         
     }
 
-   
+    void OnTriggerEnter(Collider col)
+    {
+        //all projectile colliding game objects should be tagged "Enemy" or whatever in inspector but that tag must be reflected in the below if conditional
+        if (col.gameObject.tag == "Weapon")
+        {
+            
+
+        }
+
+    }
+
+
 }
