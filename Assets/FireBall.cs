ad57e5f9e76258abe363e70277eeafb81cc094a9
LiamSlyne <liam.slyne@colorado.edu>
2019-12-14 08:37:12 -0600

    everything
    fireball weapon hit enemy spawn enemy death enemy attack pathing and more

---
+++ Assets/FireBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float dieTime = 3f;
+    public GameObject collisionExplosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, dieTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            GameObject explosion = (GameObject)Instantiate(collisionExplosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(explosion, 5f);
+            return;
+        }
+    }
+
+}
---
+++ Assets/ZombieManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieManager : MonoBehaviour
+{
+
+
+    public Transform[] m_SpawnPoints;
+    public GameObject m_ZombiePrefab;
+    private int currentNum;
+    private int totalKills = -5;
+    private int maxEnemy = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentNum = GameObject.FindGameObjectsWithTag("Zombie").Length;
+        SpawnNewEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentNum <= maxEnemy)
+        {
+
+            SpawnNewEnemy();
+            //Debug.Log("SPAWN BECAUSE LOW CURENNT NUM: ");
+            //Debug.Log(currentNum);
+        }
+
+        if(totalKills == 5)
+        {
+            maxEnemy = maxEnemy + 2;
+            Debug.Log("MAX ENEMY INCREASE");
+        }else if(totalKills == 10){
+            maxEnemy = maxEnemy + 3;
+            Debug.Log("MAX ENEMY INCREASE");
+        }
+        else if (totalKills == 15)
+        {
+            maxEnemy = maxEnemy + 3;
+            Debug.Log("MAX ENEMY INCREASE");
+        }
+        else if (totalKills == 20)
+        {
+            maxEnemy = maxEnemy + 3;
+            Debug.Log("MAX ENEMY INCREASE");
+        }
+
+        currentNum = GameObject.FindGameObjectsWithTag("Zombie").Length;
+        //Debug.Log("TOTAL KILLS: ");
+        //Debug.Log(currentNum);
+    }
+   /*
+    private void OnEnable()
+    {
+        EnemyController.OnEnemyKilled += SpawnNewEnemy;
+        currentNum--;
+        totalKills++;
+        Debug.Log("TOTAL KILLS: ");
+        Debug.Log(totalKills);
+    }
+   */
+
+    void SpawnNewEnemy()
+    {
+        totalKills++;
+        int randomNumber = Mathf.RoundToInt(Random.Range(0f, m_SpawnPoints.Length - 1));
+        Instantiate(m_ZombiePrefab, m_SpawnPoints[randomNumber].transform.position, Quaternion.identity);
+
+    }
+
+}
